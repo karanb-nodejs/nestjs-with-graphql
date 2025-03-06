6084dfe48fc21ca775e120cdf70345b963266201
karanb <karanb@hyperlinkinfosystem.net.in>
2025-03-06 13:37:36 +0530

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/', {
+      dbName: 'test',
+    }),
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
